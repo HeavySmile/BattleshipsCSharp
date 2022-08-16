515e6d7469f43f4fc97f717e085f893e74e23790
HeavySmile <lenyashubin2002@gmail.com>
2022-08-16 18:09:18 +0300

    Added basic class functionality

--- BattleshipsLib/Ship.cs
+++ BattleshipsLib/Ship.cs
@@ -3,6 +3,15 @@ namespace BattleshipsLib
 {
     public class Ship
     {
+        public List<Tile> Tiles { get; }
+        public int Size { get; }
+        public int HP { get; }
+        public Ship(List<Tile> shipTiles)
+        {
+            Tiles = shipTiles;
+            Size = shipTiles.Count;
+            HP = shipTiles.Count;
+        }
         
     }
 }
