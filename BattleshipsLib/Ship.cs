394f2b397c70444c6a39dae405eab9dc61b5f4ff
HeavySmile <lenyashubin2002@gmail.com>
2022-08-18 14:47:55 +0300

    Changed Ship class logic

--- BattleshipsLib/Ship.cs
+++ BattleshipsLib/Ship.cs
@@ -3,15 +3,30 @@ namespace BattleshipsLib
 {
     public class Ship
     {
-        public List<Tile> Tiles { get; }
-        public int Size { get; }
-        public int HP { get; }
+        private List<Tile> tiles;
+        private int size;
+        private int hp;
+        public List<Tile> Tiles
+        {
+            get { return tiles; }
+        }
+        public int Size 
+        { 
+            get { return size; }
+        }
+        public int Hp 
+        { 
+            get { return hp; } 
+        }
         public Ship(List<Tile> shipTiles)
         {
-            Tiles = shipTiles;
-            Size = shipTiles.Count;
-            HP = shipTiles.Count;
+            tiles = shipTiles;
+            size = shipTiles.Count;
+            hp = shipTiles.Count;
         }
-        
+        public void takeDamage()
+        {
+            if(hp > 0) hp--;
+        }
     }
 }
