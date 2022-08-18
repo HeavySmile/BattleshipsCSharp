40ef3f1c5b8700f2aed488419a46fe0676ddecdf
HeavySmile <lenyashubin2002@gmail.com>
2022-08-18 14:47:41 +0300

    Changed Tile class logic

--- BattleshipsLib/Tile.cs
+++ BattleshipsLib/Tile.cs
@@ -1,55 +1,125 @@
 
 namespace BattleshipsLib
 {
-    public enum TileContent { HIT, MISS, SHIP, EMPTY}
+    public enum Content { HIT, MISS, SHIP, EMPTY}
+    
+    // public class Tile
+    // {
+    //     private int x;
+    //     private int y;
+    //     private Content content;
+    //     private Ship? shipRef;
+    //     public int X 
+    //     {
+    //         get { return x;}
+    //         set { x = value;}
+    //     }
+    //     public int Y 
+    //     {
+    //         get { return y;}
+    //         set { y = value;}
+    //     }
+    //     public Content Content 
+    //     { 
+    //         get { return content; }
+    //     }
+    //     public Ship? ShipRef 
+    //     { 
+    //         get { return shipRef; }
+    //         set 
+    //         { 
+    //             shipRef = value; 
+    //             content = Content.SHIP;
+    //         }
+    //     }
+
+    //     public Tile(int x, int y)
+    //     {
+    //         this.x = x;
+    //         this.y = y;
+    //         content = Content.EMPTY;
+    //         shipRef = null;
+    //     }
+
+    //     public void takeShot()
+    //     {
+    //         if(content == Content.SHIP && shipRef != null)
+    //         {
+    //             shipRef.takeDamage();
+    //             content = Content.HIT;
+    //             shipRef = null;
+    //             return;
+    //         }
+
+    //         content = Content.MISS;
+    //     }
+    //     public void printAsFriend()
+    //     {
+    //         string output;
+    //         switch (Content)
+    //         {
+    //             case Content.HIT:
+    //                 output = "[X]";
+    //                 break;
+    //             case Content.MISS:
+    //                 output = "[M]";
+    //                 break;
+    //             case Content.SHIP:
+    //                 output = "[0]";
+    //                 break;
+    //             default:
+    //                 output = "[~]";
+    //                 break;     
+    //         }
+    //         Console.Write(output);
+    //     }
+    //     public void printAsEnemy()
+    //     {
+    //         string output;
+    //         switch (Content)
+    //         {
+    //             case Content.HIT:
+    //                 output = "[X]";
+    //                 break;
+    //             case Content.MISS:
+    //                 output = "[M]";
+    //                 break;
+    //             default:
+    //                 output = "[~]";
+    //                 break;     
+    //         }
+    //         Console.Write(output);
+    //     }
+    // }
+    
     public class Tile
     {
-        public int X { get; }
-        public int Y { get; }
-        public TileContent Content{ get; set;}
+        private int x;
+        private int y;
+
+        private Content content;
+        public int X 
+        { 
+            get { return x; }
+        }
+        public int Y 
+        { 
+            get { return y; }
+        }
+        public Content Content
+        { 
+            get { return content; }
+            set { content = value; }
+        }
         public Tile(int x, int y)
         {
-            X = x;
-            Y = y;
-            Content = TileContent.EMPTY;
+            this.x = x;
+            this.y = y;
+            this.content = Content.EMPTY;
         }
-        
-        public void printAsFriend()
+        public static bool CompareXY(Tile a, Tile b)
         {
-            string output;
-            switch (Content)
-            {
-                case TileContent.HIT:
-                    output = "[X]";
-                    break;
-                case TileContent.MISS:
-                    output = "[M]";
-                    break;
-                case TileContent.SHIP:
-                    output = "[0]";
-                    break;
-                default:
-                    output = "[~]";
-                    break;     
-            }
-            Console.Write(output);
-        }
-        public void printAsEnemy()
-        {
-            string output;
-            switch (Content)
-            {
-                case TileContent.HIT:
-                    output = "[X]";
-                    break;
-                case TileContent.MISS:
-                    output = "[M]";
-                    break;
-                default:
-                    output = "[~]";
-                    break;     
-            }
-            Console.Write(output);
+            return a.x == b.x && a.y == b.y;
         }
     }
 }
